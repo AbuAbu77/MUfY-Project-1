d645b565fb703d22002ad0098faf2d85480223f9
AbuAbu77 <muhammadabuhurairah77@gmail.com>
2026-05-25 05:23:51 +0000

    done

---
+++ Part1/ex03.py
@@ -0,0 +1,18 @@
+# Original text
+string1 = 'MaRviN THE bOt'
+
+#convert to lowercase
+string_lower = 'marvin the bot'
+
+#convert to uppercase 
+string_upper = 'MARVIN THE BOT'
+
+
+#remove spaces
+string_nospace = 'MARVINTHEBOT'
+
+# Print results
+print(string1)
+print(string_lower)
+print(string_upper)
+print(string_nospace)
---
+++ Part1/ex08.py
@@ -0,0 +1,7 @@
+# Current speed
+speed = 61
+print(speed)
+
+# : If the speed is more than 60,
+# print "Over the speed limit!"
+print("Over the speed limit !") if speed > 60 else print ("Within the speed limit !")
